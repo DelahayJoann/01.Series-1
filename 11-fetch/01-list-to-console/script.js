09bed048b3bcae5df85c25b7a5870aa3dabcd1d1
Joann Delahay <delahay.joann@gmail.com>
2020-10-08 16:51:23 +0200

    part 11 update

--- 11-fetch/01-list-to-console/script.js
+++ 11-fetch/01-list-to-console/script.js
@@ -2,5 +2,7 @@
 
 
 (() => {
-    // your code here
+    fetch('http://localhost:3000/heroes')
+    .then(response => response.json())
+    .then(data => console.log(data));
 })();
--- 11-fetch/02-list-to-template/script.js
+++ 11-fetch/02-list-to-template/script.js
@@ -1,5 +1,35 @@
-
-
 (() => {
-    // your code here
+    let target = document.getElementById('target');
+
+    let heroes = document.createElement('li');
+    let h4 = document.createElement('h4');
+    let p = document.createElement('p');
+    let name = document.createElement('strong');
+    let alterEgo = document.createElement('em');
+                        
+    heroes.setAttribute('class','hero');
+    h4.setAttribute('class','title');
+    p.setAttribute('class','powers')
+    name.setAttribute('class','name');
+    alterEgo.setAttribute('class','alter-ego');
+
+    h4.appendChild(name);
+    h4.appendChild(alterEgo);
+    heroes.appendChild(h4);
+    heroes.appendChild(p);
+    
+    fetch('http://localhost:3000/heroes')
+    .then(response => response.json())
+    .then(data => {
+        for(hero of data){
+            name.innerHTML = data['name'];
+            alterEgo.innerHTML = data['alterEgo'];
+            p.innerHTML = data['abilities'];
+        }
+        target.appendChild(heroes);
+    });
+
+
+    
+
 })();
